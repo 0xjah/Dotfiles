165fbd6d2f89e4163fcb737feddf2c85db2a2451
0xjah <ahmad45.ja4u@gmail.com>
2024-09-28 23:13:08 +0300

    first commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  { "shaunsingh/nord.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+ 
+  opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+	},
+}
